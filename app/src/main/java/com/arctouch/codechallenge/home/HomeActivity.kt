e04c0d3abd22f235b0651bb52f888fa1a2e2c8c8
Mauricio Pessoa <mauricio.ufma@gmail.com>
2018-03-07 04:55:29 -0300

    Closes #2. Using a scroll listener to implement infinite scrolling on the HomeActivity.

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.arctouch.codechallenge.home
 
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.TmdbApi
@@ -10,21 +12,95 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.home_activity.*
 
+
 class HomeActivity : BaseActivity() {
 
+    companion object {
+        val LOG_TAG = HomeActivity::class.java.simpleName
+    }
+
+    // Always start from the first page.
+    private var curPage = 1L
+    // The API docs says that the limit of pagination is 1000.
+    private var maxPages = 1000
+    // The number of pages returned by the API.
+    private var numPages = 0
+
+    private var isLoading = false
+    private var isLastPage = false
+
+    private val adapter: HomeAdapter by lazy {
+        HomeAdapter(ArrayList())
+    }
+
+    // Activity lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                val moviesWithGenres = it.results.map { movie ->
-                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
-                }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
-                progressBar.visibility = View.GONE
+        setupRV()
+        // Load the movies for the first time (First page)
+        loadMovies()
+    }
+
+    // TODO - Save Activity state to avoid multiple downloads.
+
+    //end section
+
+
+    /**
+     * Setup the Recycler View, adding the LayoutManager and setting the scroll listener.
+     */
+    private fun setupRV() {
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.adapter = adapter
+        // Listen to the scrolls, and make the magic happen. Infinite scroll.
+        recyclerView.addOnScrollListener(object: RvScrollListener(layoutManager) {
+            override fun isLoading(): Boolean = isLoading
+            override fun isLastPage(): Boolean = isLastPage
+            override fun getTotalPageCount(): Int = if (numPages > 0) numPages else maxPages
+
+            override fun loadMoreItems() {
+                loadMovies()
             }
+        })
+    }
+
+    /**
+     * Method that will fetch the movies from the API, and display the results on the RV.
+     */
+    private fun loadMovies() {
+        if (curPage == 1L)
+            progressBar.visibility = View.VISIBLE
+        else if (curPage > 1L)
+            recyclerView.post { adapter.addFooter()  }
+
+        if (!isLastPage) {
+            isLoading = true
+            api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, curPage, TmdbApi.DEFAULT_REGION)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        val moviesWithGenres = it.results.map { movie ->
+                            movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+                        }
+
+                        // Check if we're past the first page. Which means there's a footer to be removed.
+                        if (curPage > 1L) adapter.removeFooter()
+
+                        // Check if it's the last page
+                        isLastPage = it.totalPages <= it.page
+                        // Increment the page counter if it's not the last page yet.
+                        if (!isLastPage)
+                            curPage = it.page + 1L
+                        // Insert the results on the RV.
+                        adapter.addAll(moviesWithGenres)
+
+                        isLoading = false
+                        progressBar.visibility = View.GONE
+                    }
+        }
     }
 }
--- app/src/main/java/com/arctouch/codechallenge/home/HomeAdapter.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeAdapter.kt
@@ -1,6 +1,7 @@
 package com.arctouch.codechallenge.home
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.movie_item.view.*
 
-class HomeAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter(private val movies: MutableList<Movie>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val movieImageUrlBuilder = MovieImageUrlBuilder()
 
@@ -26,15 +27,105 @@ class HomeAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<HomeAd
                 .load(movie.posterPath?.let { movieImageUrlBuilder.buildPosterUrl(it) })
                 .apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
                 .into(itemView.posterImageView)
+
+            itemView.setOnClickListener {
+                onClick(movie)
+            }
+        }
+
+        private fun onClick(movie: Movie) {
+            Log.d(LOG_TAG, "onClick()")
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
-        return ViewHolder(view)
+    // Dummy ViewHolder for the footer loading progress bar.
+    class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    // Flag that will indicate wheter the footer has been added or not.
+    private var isLoadingOnFooter = false
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            ITEM_DEFAULT -> MovieViewHolder( LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false) )
+            else -> LoadingViewHolder( LayoutInflater.from(parent.context).inflate(R.layout.loading_item, parent, false) )
+        }
     }
+
+    override fun getItemViewType(position: Int): Int =
+            if (position == movies.size - 1 && isLoadingOnFooter) ITEM_LOADING else ITEM_DEFAULT
 
     override fun getItemCount() = movies.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(movies[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val movie = movies[position]
+
+        // If we decide to add more view types later, add them here.
+        when(getItemViewType(position)) {
+            ITEM_DEFAULT -> (holder as MovieViewHolder).bind(movie)
+            // The ITEM_LOADING will basically show the progress bar, so I'm not binding it here.
+        }
+    }
+
+    // Adapter helper methods (Sort of a CRUD)
+
+    fun add(movie: Movie) {
+        movies.add(movie)
+        notifyItemInserted(movies.size - 1)
+    }
+
+    fun addAll(newMovies: List<Movie>) {
+        for (movie in newMovies) {
+            add(movie)
+        }
+    }
+
+    fun remove(movie: Movie) {
+        val pos = movies.indexOf(movie)
+
+        if (pos > -1) {
+            movies.removeAt(pos)
+            notifyItemRemoved(pos)
+        }
+    }
+
+    fun removeAtPos(position: Int) {
+        if (position < movies.size) {
+            movies.removeAt(position)
+            notifyItemRemoved(position)
+        }
+    }
+
+    fun clearAll() {
+        isLoadingOnFooter = false
+
+        while (movies.size > 0) {
+            remove(movies[0])
+        }
+    }
+
+    fun addFooter() {
+        isLoadingOnFooter = true
+
+        // Add a dummy movie object on the end of the list.
+        add(Movie())
+    }
+
+    fun removeFooter() {
+        isLoadingOnFooter = false
+
+        // Check if there are any items on the list.
+        if (!movies.isEmpty()) {
+            val pos = movies.size - 1
+
+            removeAtPos(pos)
+        }
+    }
+
+    companion object {
+        val LOG_TAG = HomeAdapter::class.java.simpleName
+
+        private const val ITEM_DEFAULT = 0
+        private const val ITEM_LOADING = 1
+    }
 }
